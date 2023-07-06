b63116f6bb5c9a745eb70301114485ffa10a8693
rahulparmar8189 <84504141+rahulparmar8189@users.noreply.github.com>
2023-07-06 23:04:17 +0530

    After reaching home some improvements done

--- New_page.dart
+++ New_page.dart
@@ -13,24 +13,44 @@ class _NewPageState extends State<NewPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Profile"),
+        backgroundColor: Color.fromARGB(255, 4, 93, 84),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            // Container(
-            //   child: Text('hey'),
-            // )
-            Container(
-              height: 200,
-              child: Image.asset('assests/images/ram.png',fit: BoxFit.cover,),
-            ),
-            Container(
-              child: Text("Rahul"),
-            ),
-            Container(
-              child: Text("rahulparmar8189@gmail.com"),
-            )
-          ],
+      body: Center(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          physics: BouncingScrollPhysics(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Container(
+              //   child: Text('hey'),
+              // )
+              Container(
+                height: 150,
+                child: Image.asset(
+                  'assests/images/ram.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              SizedBox(
+                height: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "Rahul",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text("rahulparmar8189@gmail.com",
+                        style: TextStyle(fontWeight: FontWeight.bold))
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
